911c5be71e59aa95e3f0a648f5bb8998a1076bc7
Saish Dharvotkar <saish.dharvotkar@c02rc01ag8wp.myob.myobcorp.net>
2019-10-02 20:49:06 +1300

    Added first test iteration of bash script for mass-clone

---
+++ mass_cloning_jutsu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Running mass clone script..."
+
+repoArray=(
+    git@github.com:MYOB-Technology/PS-Collaboration-Service.git
+    git@github.com:MYOB-Technology/ps-documentmanager-service.git
+    git@github.com:MYOB-Technology/ps-document-module.git
+    git@github.com:MYOB-Technology/ps-collaboration-portal-module.git
+    git@github.com:MYOB-Technology/ps-collaboration-document-module.git
+    git@github.com:MYOB-Technology/ps-practice-task-module.git
+    git@github.com:MYOB-Technology/ps-collaboration-components.git
+)
+
+for element in ${repoArray[@]}; do
+    echo "Clonning Repo $element"
+    git clone $element
+done
