86e86b60603ef7b5a2b4f50b985e8d3775dd07d6
sishydishy <saish.dharvotkar@myob.com>
2020-01-11 00:32:42 +1300

    Did some clean up and added a way to create automated directory and also now it does a check for particular commands

--- mass_cloning_jutsu.sh
+++ mass_cloning_jutsu.sh
@@ -4,9 +4,32 @@ echo "=============================================="
 echo "Welcome To The Mass Github Cloning Tool"
 echo "=============================================="
 
+echo "Checking if you have brew..."
+if hash brew; then
+    echo "...brew is installed"
+    else
+        echo "...brew is installed"
+        echo "Installing brew"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+fi
+
+echo "Checking if you have jq..."
+
+if hash jq; then
+    echo "...jq is installed"
+    else
+        echo "...jq is installed"
+        echo "Installing jq via brew"
+        brew install jq
+fi
+
 echo "Before we continue please make sure you have read the README for this repo"
 
-# sleep 5
+echo "Creating Project Directory..."
+
+DIRECTORY=${HOME}/Source
+
+echo "Project Directory is located at $DIRECTORY"
 
 read -p "Please enter you Github OAUTH Token: " token
 
@@ -16,11 +39,17 @@ echo "Running mass clone script..."
 
 npm i axios
 
+
 repoArray=(`node graphQL_jutsu.js | jq .[].search.edges[].node.sshUrl`)
 
+if [ ! -d "$DIRECTORY" ];then
+    mkdir $DIRECTORY
+    cd $DIRECTORY
+else
+    cd $DIRECTORY
+fi
+
 for element in ${repoArray[@]}; do
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_rsa
     repo=`echo "$element" | tr -d '"'`
     git clone $repo
 done
