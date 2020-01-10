9b446fcb5c9e46140360839265908e8e50ab06b3
sishydishy <saish.dharvotkar@myob.com>
2020-01-10 23:30:17 +1300

    Fixed cloning script bug due to quotes on git clone

--- mass_cloning_jutsu.sh
+++ mass_cloning_jutsu.sh
@@ -6,35 +6,37 @@ echo "=============================================="
 
 echo "Before we continue please make sure you have read the README for this repo"
 
+# sleep 5
+
 read -p "Please enter you Github OAUTH Token: " token
 
 export token
 
-
-
-
-
-
 echo "Running mass clone script..."
 
-# repoArray=(
-#     git@github.com:MYOB-Technology/PS-Collaboration-Service.git
-#     git@github.com:MYOB-Technology/ps-documentmanager-service.git
-#     git@github.com:MYOB-Technology/ps-document-module.git
-#     git@github.com:MYOB-Technology/ps-collaboration-portal-module.git
-#     git@github.com:MYOB-Technology/ps-collaboration-document-module.git
-#     git@github.com:MYOB-Technology/ps-practice-task-module.git
-#     git@github.com:MYOB-Technology/ps-collaboration-components.git
-# )
+repoArray=(
+    git@github.com:MYOB-Technology/PS-Collaboration-Service.git
+    git@github.com:MYOB-Technology/ps-documentmanager-service.git
+    git@github.com:MYOB-Technology/ps-document-module.git
+    git@github.com:MYOB-Technology/ps-collaboration-portal-module.git
+    git@github.com:MYOB-Technology/ps-collaboration-document-module.git
+    git@github.com:MYOB-Technology/ps-practice-task-module.git
+    git@github.com:MYOB-Technology/ps-collaboration-components.git
+)
 
-# for element in ${repoArray[@]}; do
-#     echo "Clonning Repo $element"
-#     git clone $element
-# done
+
+
+
 npm i axios
 
-node graphQL_jutsu.js | jq .[].search.edges[].node.sshUrl
+#repoArray=(`node graphQL_jutsu.js | jq .[].search.edges[].node.sshUrl`)
 
+for element in ${repoArray[@]}; do
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_rsa
+    echo "$element" | tr -d '"'
+    git clone $element
+done
 
 # echo "hello"
 
