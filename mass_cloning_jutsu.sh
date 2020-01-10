71e75d889fda67b463cafb4583264fbe962ef085
sishydishy <saish.dharvotkar@myob.com>
2020-01-10 23:34:28 +1300

    FIXED BUGS THAT WERE CAUSING CHAOS

--- mass_cloning_jutsu.sh
+++ mass_cloning_jutsu.sh
@@ -14,27 +14,27 @@ export token
 
 echo "Running mass clone script..."
 
-repoArray=(
-    git@github.com:MYOB-Technology/PS-Collaboration-Service.git
-    git@github.com:MYOB-Technology/ps-documentmanager-service.git
-    git@github.com:MYOB-Technology/ps-document-module.git
-    git@github.com:MYOB-Technology/ps-collaboration-portal-module.git
-    git@github.com:MYOB-Technology/ps-collaboration-document-module.git
-    git@github.com:MYOB-Technology/ps-practice-task-module.git
-    git@github.com:MYOB-Technology/ps-collaboration-components.git
-)
+# repoArray=(
+#     git@github.com:MYOB-Technology/PS-Collaboration-Service.git
+#     git@github.com:MYOB-Technology/ps-documentmanager-service.git
+#     git@github.com:MYOB-Technology/ps-document-module.git
+#     git@github.com:MYOB-Technology/ps-collaboration-portal-module.git
+#     git@github.com:MYOB-Technology/ps-collaboration-document-module.git
+#     git@github.com:MYOB-Technology/ps-practice-task-module.git
+#     git@github.com:MYOB-Technology/ps-collaboration-components.git
+# )
 
 
 
 
 npm i axios
 
-#repoArray=(`node graphQL_jutsu.js | jq .[].search.edges[].node.sshUrl`)
+repoArray=(`node graphQL_jutsu.js | jq .[].search.edges[].node.sshUrl`)
 
 for element in ${repoArray[@]}; do
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
-    echo "$element" | tr -d '"'
+    #name=`echo "$element" | tr -d '"'`
     git clone $element
 done
 
